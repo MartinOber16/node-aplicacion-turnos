4a35f607c908279190bfde7317c1e062247fe38d
Martin Ober <martinober@MacBook-Air-de-Martin.local>
2020-12-16 21:46:14 -0300

    Primer commit

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,38 @@
+// Comando para establecer la nueva conexion
+var socket = io();
+
+var  label = $('#lblNuevoTicket');
+
+// on: escuchar sucesos
+socket.on('connect', function(){
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function(){
+    console.log('Desconectado del servidor');
+});
+
+var searchParams = new URLSearchParams(window.location.search);
+
+if(!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+} 
+
+var escritorio = searchParams.get('escritorio');
+var label = $('small');
+
+$('h1').text('Escritorio ' + escritorio);
+
+$('button').on('click', function(){
+    socket.emit('atenderTicket', {escritorio: escritorio}, function(resp) {
+        console.log(resp);
+        if(resp === 'No hay tickets') {
+            alert(resp);
+            label.text(resp);
+            return;
+        }
+
+        label.text('Ticket ' + resp.numero);
+    });
+});
